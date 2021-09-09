8e096b47b73cf89aeb0810fd5b4fc30d4c2c9075
Azullon_notebook <snick159ers@gmail.com>
2021-09-09 16:17:04 +0300

    [del] visualisation tables

--- src/App.vue
+++ src/App.vue
@@ -4,42 +4,10 @@
   </h2>
   <MyTable :data-manager="dataManager1"></MyTable>
 
-  <h3>
-    Entered data:
-  </h3>
-  <table>
-    <tbody>
-      <tr v-for="(row, rowIndex) in tableData1" :key="'row-' + rowIndex">
-        <td v-for="(cell, columnIndex) in row" :key="`cell-${row}-${columnIndex}`">
-          <div>
-            {{ cell }}
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-
   <h2>
     Table 2
   </h2>
   <MyTable :data-manager="dataManager2" @cell-input="onInput"></MyTable>
-
-  <button @click="tableData2[0][0] = 'abcd'">dnakd</button>
-
-  <h3>
-    Entered data:
-  </h3>
-  <table>
-    <tbody>
-      <tr v-for="(row, rowIndex) in tableData2" :key="'row-' + rowIndex">
-        <td v-for="(cell, columnIndex) in row" :key="`cell-${row}-${columnIndex}`">
-          <div>
-            {{ cell }}
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
 </template>
 
 <script lang="ts">
