e330632a77574a4e56697a7ad06ce25b76d20b58
Azullon_notebook <snick159ers@gmail.com>
2021-09-14 12:07:20 +0300

    [upd] simplified demo

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,15 @@
 <template>
-  <h2>
-    Table 1
-  </h2>
-  <MyTable :data="tableData"></MyTable>
+  <h1>Sample table</h1>
+  <MyTable :data="tableData" />
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive } from 'vue'
+<script lang="ts" setup>
+import { reactive } from 'vue'
 import MyTable from './components/MyTable.vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    MyTable,
-  },
-  setup() {
-    // use case 1
-    const tableData = reactive([
-      ['', 'Height', 'Burst'],
-      ['S', '156-160', '74-90'],
-      ['М', '159-164', '88-96'],
-    ])
-
-    return { tableData }
-  },
-})
+const tableData = reactive([
+  ['', 'Height', 'Burst'],
+  ['S', '156-160', '74-90'],
+  ['М', '159-164', '88-96'],
+])
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
